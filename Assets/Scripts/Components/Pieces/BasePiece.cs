381dab431cbb87dae3de97225375851bdc3db254
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-07-29 12:58:41 +0700

    WIP - [#167556016] Added pieces being interactable and draws outline for valid cell movement

--- Assets/Scripts/Components/Pieces/BasePiece.cs
+++ Assets/Scripts/Components/Pieces/BasePiece.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public abstract class BasePiece : EventTrigger
 {
@@ -12,6 +13,9 @@ public abstract class BasePiece : EventTrigger
 
     protected RectTransform rectTransform = null;
     protected PieceManager pieceManager;
+
+    protected Vector3Int movement = Vector3Int.one;
+    protected List<Cell> highlightedCells = new List<Cell>();
 
     public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
     {
@@ -33,4 +37,85 @@ public abstract class BasePiece : EventTrigger
         transform.position = newCell.transform.position;
         gameObject.SetActive(true);
     }
+
+    private void CreateCellPath(int xDirection, int yDirection, int movement)
+    {
+        // Target position
+        int currentX = currentCell.boardPosition.x;
+        int currentY = currentCell.boardPosition.y;
+
+        // Check each cell
+        for (int i = 1; i <= movement; i++)
+        {
+            currentX += xDirection;
+            currentY += yDirection;
+
+            // TODO: Get the state of the target cell
+
+            // Add to list
+            highlightedCells.Add(currentCell.board.allCells[currentX, currentY]);
+        }
+    }
+
+    protected virtual void CheckPathing()
+    {
+        // Horizontal
+        CreateCellPath(1, 0, movement.x);
+        CreateCellPath(-1, 0, movement.x);
+
+        // Vertical
+        CreateCellPath(0, 1, movement.y);
+        CreateCellPath(0, -1, movement.y);
+
+        // Upper diagonal
+        CreateCellPath(1, 1, movement.z);
+        CreateCellPath(-1, 1, movement.z);
+
+        // Lower diagonal
+        CreateCellPath(-1, -1, movement.z);
+        CreateCellPath(1, -1, movement.z);
+    }
+
+    protected void ShowCells()
+    {
+        foreach (Cell cell in highlightedCells)
+            cell.outlineImage.enabled = true;
+    }
+
+    protected void ClearCells()
+    {
+        foreach (Cell cell in highlightedCells)
+            cell.outlineImage.enabled = false;
+
+        highlightedCells.Clear();
+    }
+
+    #region Events
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+
+        // Test for cells
+        CheckPathing();
+
+        // Show valid cells
+        ShowCells();
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+
+        // Follow pointer
+        transform.position += (Vector3)eventData.delta;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        // Hide
+        ClearCells();
+    }
+    #endregion
 }
