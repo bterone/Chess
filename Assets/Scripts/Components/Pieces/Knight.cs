6b2286d6982cbc949d2fa64f9664f0f299cb9853
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-08-11 15:27:06 +0700

    [#167826096] Added Knight Movement

--- Assets/Scripts/Components/Pieces/Knight.cs
+++ Assets/Scripts/Components/Pieces/Knight.cs
@@ -12,4 +12,41 @@ public class Knight : BasePiece
         // TODO: Movement
         GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Knight");
     }
+
+    private void CreateCellPath(int flipper)
+    {
+        // Target position
+        int currentX = currentCell.boardPosition.x;
+        int currentY = currentCell.boardPosition.y;
+
+        // Left
+        MatchesState(currentX - 2, currentY + (1 * flipper));
+
+        // Upper left
+        MatchesState(currentX - 1, currentY + (2 * flipper));
+
+        // Upper right
+        MatchesState(currentX + 1, currentY + (2 * flipper));
+
+        // Right
+        MatchesState(currentX + 2, currentY + (1 * flipper));
+    }
+
+    protected override void CheckPathing()
+    {
+        // Draw top half
+        CreateCellPath(1);
+
+        // Draw bottom half
+        CreateCellPath(-1);
+    }
+
+    private void MatchesState(int targetX, int targetY)
+    {
+        CellState cellState = CellState.None;
+        cellState = currentCell.board.ValidateCell(targetX, targetY, this);
+
+        if (cellState != CellState.Friendly && cellState != CellState.OutOfBounds)
+            highlightedCells.Add(currentCell.board.allCells[targetX, targetY]);
+    }
 }
