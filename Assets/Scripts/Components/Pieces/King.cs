3bd805285fb2ab65d1377bb406760def9d6c5e96
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-09-01 16:35:46 +0700

    [#167556012] Added ability to castle the king

--- Assets/Scripts/Components/Pieces/King.cs
+++ Assets/Scripts/Components/Pieces/King.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 public class King : BasePiece
 {
+    private Rook leftRook = null;
+    private Rook rightRook = null;
+
     public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
     {
         // Base setup
@@ -18,5 +21,84 @@ public class King : BasePiece
         base.Kill();
 
         pieceManager.isKingAlive = false;
+    }
+
+    protected override void CheckPathing()
+    {
+        base.CheckPathing();
+
+        // Right
+        rightRook = GetRook(1, 3);
+
+        // Left
+        leftRook = GetRook(-1, 4);
+    }
+
+    protected override void Move()
+    {
+        base.Move();
+
+        if(CanCastle(leftRook))
+            leftRook.Castle();
+
+        if(CanCastle(rightRook))
+            rightRook.Castle();
+    }
+
+    private bool CanCastle(Rook rook)
+    {
+        if (rook == null)
+            return false;
+
+        if(rook.castleTriggerCell != currentCell)
+            return false;
+
+        return true;
+    }
+
+    private Rook GetRook(int direction, int count)
+    {
+        // Has the king moved?
+        if (!isFirstMove)
+            return null;
+
+        // Get King Position
+        int currentX = currentCell.boardPosition.x;
+        int currentY = currentCell.boardPosition.y;
+
+        // Go through the cells if pieces are there inbetween rook and king
+        for(int i = 1; i < count; i++)
+        {
+            // Direction can be positive or negative for right or left
+            int offsetX = currentX + (i * direction);
+            CellState cellState = currentCell.board.ValidateCell(offsetX, currentY, this);
+
+            if(cellState != CellState.Free)
+                return null;
+        }
+
+        // Try and get rook
+        Cell rookCell = currentCell.board.allCells[currentX + (count * direction), currentY];
+        Rook rook = null;
+
+        // Cast to see if Rook and Friendly
+        if(rookCell.currentPiece != null)
+        {
+            if(rookCell.currentPiece is Rook)
+                rook = (Rook)rookCell.currentPiece;
+        }
+
+        // Return if no rook
+        if(rook == null)
+            return null;
+
+        // Check color and movement
+        if(rook.color != color || !rook.isFirstMove)
+            return null;
+
+        // Add Castle trigger to movement
+        highlightedCells.Add(rook.castleTriggerCell);
+
+        return rook;
     }
 }
--- Assets/Scripts/Components/Pieces/Rook.cs
+++ Assets/Scripts/Components/Pieces/Rook.cs
@@ -3,6 +3,10 @@ using UnityEngine.UI;
 
 public class Rook : BasePiece
 {
+    [HideInInspector]
+    public Cell castleTriggerCell = null;
+    private Cell castleCell = null;
+
     public override void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
     {
         // Base setup
@@ -11,5 +15,38 @@ public class Rook : BasePiece
         // Rook stuff
         movement = new Vector3Int(7, 7, 0);
         GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Rook");
+    }
+
+    public override void Place(Cell newCell)
+    {
+        base.Place(newCell);
+
+        // Trigger cell
+        // Sets triggerOffset on whether currentcell is over on left or right
+        int triggerOffset = currentCell.boardPosition.x < 4 ? 2 : -1;
+        castleTriggerCell = SetCell(triggerOffset);
+
+        // Castle cell
+        int castleOffset = currentCell.boardPosition.x < 4 ? 3 : -2;
+        castleCell = SetCell(castleOffset);
+    }
+
+    public void Castle()
+    {
+        // Set new target
+        targetCell = castleCell;
+
+        // Move
+        Move();
+    }
+
+    private Cell SetCell(int offset)
+    {
+        // New Position
+        Vector2Int newPosition = currentCell.boardPosition;
+        newPosition.x += offset;
+
+        // Return
+        return currentCell.board.allCells[newPosition.x, newPosition.y];
     }
 }
